992ac7c853b1299c8c090a124122118c819abe57
AlexanderV <avaughan@barefootsolutions.com>
2018-10-12 10:26:11 -0700

    Query for pokemon by name and type: RETURN ARRAY

--- public/index.php
+++ public/index.php
@@ -1,36 +1,33 @@
 <?php require_once("../includes/db_connection.php") ?>
 <?php require_once("../includes/functions.php") ?>
+<?php require_once("search.php") ?>
+
 
 <?php
 // SUPPRESS WARNINGS
 error_reporting(E_ERROR | E_PARSE);
 
-
-
 if(isset($_GET["search"])){
-    $searchQuery = $_GET["search"];
+    $searchParam = $_GET["search"];
+    //echo "received query: " . $searchParam . "<br>";
 
-    echo "recieved query: " . $searchQuery;
+    // Find result to return to Angular App
+    $resultArray = array();
+
+    // http://localhost:8888/?search=Bulbasaur
+    searchPokemon($connection,$searchParam,$resultArray);
 
 } else {
     // Parse JSON
-
-    //read the json file contents
     $jsonRaw = file_get_contents('../data/pokemon.json');
 
     $jsonIterator = new RecursiveIteratorIterator(
         new RecursiveArrayIterator(json_decode($jsonRaw, TRUE)),
         RecursiveIteratorIterator::SELF_FIRST);
 
-//parsePokemon($jsonIterator, $connection);
+//    parsePokemon($jsonIterator, $connection);
 
 }
-
-
-
-
-
-
 ?>
 
 
--- public/search.php
+++ public/search.php
@@ -1,3 +1,57 @@
 <?php
 
-echo "at search";
+function atSearch() {
+    echo "at search";
+}
+
+function searchPokemon($connection, $param, $resultArray){
+    // Search for names
+    $param = mysqli_real_escape_string($connection,$param);
+    $query = "SELECT name FROM pokemon WHERE name LIKE '%{$param}%'";
+    $result = mysqli_query($connection, $query);
+
+    //echo "Result: <br>" . $result;
+    while($row = mysqli_fetch_assoc($result)){
+        foreach($row as $name => $value){
+            //print "$name: $value <br>";
+            array_push($resultArray,$value);
+        }
+    }
+    searchPokemonType($connection, $param, $resultArray);
+}
+
+function searchPokemonType($connection, $param, $resultArray){
+    $count = 0;
+    // Search for types
+    // If type match, grab parent_id and grab name
+    $param = mysqli_real_escape_string($connection,$param);
+
+    $query = "SELECT pokemon_id FROM pokemon_type WHERE type LIKE '%{$param}%'";
+
+    $result = mysqli_query($connection, $query);
+
+//    echo "Result Type: <br>";
+
+    while($row = mysqli_fetch_assoc($result)){
+        foreach($row as $pokemon_id => $id){
+            //print "$id: $value\t";
+            array_push($resultArray,selectPokemonByType($connection,$id));
+        }
+        $count++;
+    }
+
+    print_r($resultArray);
+}
+
+function selectPokemonByType($connection, $id) {
+    //iterate through array, query for each id, and return result
+    $query = "SELECT name FROM pokemon WHERE id={$id}";
+    $result = mysqli_query($connection, $query);
+
+    while($row = mysqli_fetch_assoc($result)){
+        foreach($row as $name => $value){
+            //print "$name: $value\t";
+            return $value;
+        }
+    }
+}
